e3c431948588a647646794a012a0c9028a257dc6
Rudraneel's Macbook Air <rudraneelsmacbookair@Rudraneelss-MacBook-Air.local>
2025-09-06 14:25:03 +0530

    Add GitHub Actions for Lean 3 and Coq; fix Coq proof script (Qabs import, N0 nat, faster vm_compute).

--- proofs/lean_coq/check_and_conclude_exact_from_user.v
+++ proofs/lean_coq/check_and_conclude_exact_from_user.v
@@ -1,4 +1,5 @@
-Require Import QArith.
+From Stdlib Require Import QArith ZArith Lia.
+From Stdlib Require Import Qabs.
 Open Scope Q_scope.
 
 Definition C_lower : Q := (52141067576471723699354534458178559415185013027024167533779#(5000000000000000000000000000000000000000000000000000000000)).
@@ -12,26 +13,30 @@ Definition E_low_a : Q := (30000012715657549122741424442739415821318358976602840
 Definition E_high_a : Q := (300000127156575491227414244427394120457#(100000000000000000000000000000000000000)).
 Definition E_low_b : Q := (287681228157235084767676821784943595901222280627836427268701#(1000000000000000000000000000000000000000000000000000000000000)).
 Definition E_high_b : Q := (57536245631447016953535364356988719180244456125567285525463#(200000000000000000000000000000000000000000000000000000000000)).
-Definition N0 := 1000000.
+Definition N0 : nat := 1000000%nat.
 
 Goal (C_upper - C_lower <= total_error)%Q.
 Proof.
   unfold C_upper, C_lower, total_error.
-  compute.
-  reflexivity.
+  unfold Qle; simpl.
+  (* Turn the Z comparison into a boolean and compute it fast to avoid heavy proof search *)
+  apply Z.leb_le.
+  vm_compute; reflexivity.
 Qed.
 
-Variable C_N : nat -> Q.
-Variable C_mean : Q.
-Variable union_bad tail_mass C_half : Q.
-Hypothesis reduction_correct : forall N : nat, (N >= N0)%nat -> Qabs (C_N N - C_mean) <= (union_bad + tail_mass + C_half)%Q.
-Hypothesis interval_bounds : (C_lower <= C_mean)%Q /\ (C_mean <= C_upper)%Q.
-Hypothesis sum_error_ok : (union_bad + tail_mass + C_half <= total_error)%Q.
+Section Certificate.
+  Variable C_N : nat -> Q.
+  Variable C_mean : Q.
+  (* Using the constants union_bad, tail_mass, C_half defined above *)
+  Hypothesis reduction_correct : forall N : nat, (N >= N0)%nat -> Qabs (C_N N - C_mean) <= (union_bad + tail_mass + C_half)%Q.
+  Hypothesis interval_bounds : (C_lower <= C_mean)%Q /\ (C_mean <= C_upper)%Q.
+  Hypothesis sum_error_ok : (union_bad + tail_mass + C_half <= total_error)%Q.
 
-Theorem final_certificate : forall N : nat, (N >= N0)%nat -> Qabs (C_N N - C_mean) <= total_error.
-Proof.
-  intros N HN.
-  apply Qle_trans with (r2 := (union_bad + tail_mass + C_half)%Q).
-  - apply reduction_correct; assumption.
-  - apply sum_error_ok.
-Qed.
+  Theorem final_certificate : forall N : nat, (N >= N0)%nat -> Qabs (C_N N - C_mean) <= total_error.
+  Proof.
+    intros N HN.
+    eapply Qle_trans.
+    - apply reduction_correct; assumption.
+    - apply sum_error_ok.
+  Qed.
+End Certificate.
